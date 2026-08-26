833e00055151f1a37d9acf6ecb8ea4b7a3e92c0e
jaydeep74129-ai <jaydeep74129@gmail.com>
2026-08-26 10:24:57 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE='tifinity-web-v1';
+const APP_SHELL=['./','./index.html','./manifest.webmanifest','./favicon.png','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET') return;
+  const url=new URL(req.url);
+  if(url.origin===self.location.origin){
+    event.respondWith(caches.match(req).then(cached=>cached||fetch(req).then(res=>{
+      const copy=res.clone();
+      caches.open(CACHE).then(c=>c.put(req,copy)).catch(()=>{});
+      return res;
+    }).catch(()=>caches.match('./index.html'))));
+  }
+});
